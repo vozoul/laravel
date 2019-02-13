a1cc6c6d3125fbb15d7713ab6d52e9d6e4732ce3
Vince <vozoul@gmail.com>
2019-02-13 11:31:38 +0100

    modif title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Hello World !
+                    Salut KevinA !!
                 </div>
 
                 <div class="links">
