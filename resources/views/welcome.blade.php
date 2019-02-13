654b8cdf0aa6f69b1530138a58e69e1dcf4d44d4
Vince <vozoul@gmail.com>
2019-02-13 11:32:44 +0100

    erreur sur title corriger

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Salut KevinA !!
+                    Salut Kevin !!
                 </div>
 
                 <div class="links">
