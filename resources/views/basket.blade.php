8611a5a73e903d2f36d355099627233a48a04444
Vince <vozoul@gmail.com>
2019-02-21 09:49:03 +0100

    return

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -64,17 +64,24 @@
         </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-                <div class="top-right links">
-                        <a href="{{ url('/home') }}">Home</a>
-                        <a href="{{ route('login') }}">Login</a>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
 
-                            <a href="{{ route('register') }}">Register</a>
-                </div>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
+        @endif
 
-            <div class="content">
+        <div class="content">
                 <div class="title m-b-md">
-                    Fiche Produit
+                    Panier
                 </div>
 
                 <div class="links">
